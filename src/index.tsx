adaa82fafb0b25691ad8150793d81031a62717ec
Higor <higorpiresoliveira@gmail.com>
2023-01-04 11:22:16 -0300

    fix: remove featurehighlight

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 export * from 'react-native-paper';
 export * from 'react-hook-form';
 
-export { default as FeatureHighlight } from 'react-native-ui-lib/featureHighlight';
 export { default as AnimatedImage } from 'react-native-ui-lib/animatedImage';
 export { default as ColorPalette } from 'react-native-ui-lib/colorPalette';
 export { default as ColorPicker } from 'react-native-ui-lib/colorPicker';
